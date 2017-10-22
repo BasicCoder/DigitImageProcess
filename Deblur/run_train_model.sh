777430ca3b6213f883319ad61cfa393541e38d4b
BasicCoder <abasiccoder@gmail.com>
2017-10-22 17:49:43 +0800

    update Deblur

---
+++ Deblur/run_train_model.sh
@@ -0,0 +1,1 @@
+cat fileListToProcess.txt | python processWholeImage.py --model_def ../BMVC_nets/S14_19_200.deploy --pretrained_model ../BMVC_nets/S14_19_FQ_178000.model --out_scale 1.0  --extract_layer CONVX_15 --output_path ./out/ --border 25  --tile_resolution 150 --suffix _out --gpu
---
+++ Deblur/test.cmd
@@ -0,0 +1,1 @@
+C:\Anaconda2\python.exe E:\BMVC\BMVC_scripts\processWholeImage.py --model_def E:\BMVC\BMVC_nets\S14_19_200.deploy --pretrained_model E:\BMVC\BMVC_nets\S14_19_real_130000.model --out_scale 1 --extract_layer CONVX_15 --border 25 --tile_resolution 150
